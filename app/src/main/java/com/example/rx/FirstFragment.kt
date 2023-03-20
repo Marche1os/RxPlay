44470250b8b7682f11b7b0df18006cd415074970
marche1os <kolpmark@yandex.ru>
2023-03-20 19:38:43 +0300

    added some rx methods

--- app/src/main/java/com/example/rx/FirstFragment.kt
+++ app/src/main/java/com/example/rx/FirstFragment.kt
@@ -11,9 +11,15 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Predicate
 import io.reactivex.rxjava3.schedulers.Schedulers
+import rx_api.createMergedObservables
 import rx_api.createObservable
+import rx_api.createObservableFromIterable
 import rx_api.schedule
+import rx_api.zipTwoObservables
+import java.util.Calendar
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 /**
@@ -43,11 +49,90 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            init()
+            takeUntil()
         }
     }
 
-    fun init() {
+    fun takeUntil() {
+        val firstDecade: Observable<Int> =
+            createObservableFromIterable(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+        val secondDecade: Observable<Int> =
+            createObservableFromIterable(listOf(11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
+
+        val zipped = zipTwoObservables(firstDecade, secondDecade) { t1, t2 ->
+            (t1 + t2)
+        }
+
+        val stopPredicate = Predicate<Int> { number -> number < 15 }
+
+        zipped.takeUntil(stopPredicate)
+            .schedule(
+                onNext = { result ->
+                    binding.textviewFirst.apply {
+                        text = "$text $result"
+                    }
+                },
+                onError = ::logError
+            )
+    }
+
+    fun zip() {
+        val firstDecade: Observable<Int> =
+            createObservableFromIterable(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+        val secondDecade: Observable<Int> =
+            createObservableFromIterable(listOf(11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
+
+        val zipped = zipTwoObservables(firstDecade, secondDecade) { t1, t2 ->
+            (t1 + t2).toString()
+        }
+
+        zipped.schedule(
+            onNext = { result ->
+                binding.textviewFirst.apply {
+                    text = "$text $result"
+                }
+            },
+            onError = ::logError
+        )
+    }
+
+    fun merge() {
+        val firstDecade: Observable<Int> =
+            createObservableFromIterable(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+        val secondDecade: Observable<Int> =
+            createObservableFromIterable(listOf(11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
+
+        val completedSource = createMergedObservables(firstDecade, secondDecade)
+        completedSource.schedule(
+            onNext = { item ->
+                binding.textviewFirst.apply {
+                    text = "$text $item"
+                }
+            },
+            onError = ::logError
+        )
+    }
+
+    fun buffer() {
+        val observable =
+            createObservableFromIterable(listOf("0", "1", "2", "3", "4", "5", "6", "7", "8"))
+
+        observable
+            .buffer(4)
+            .schedule(
+                onNext = { items ->
+                    val currentText = binding.textviewFirst.text
+
+                    binding.textviewFirst.text = "$currentText ${items.joinToString()}\n"
+                },
+                onError = ::logError
+            )
+    }
+
+    fun simpleObservable() {
         val observable: Observable<Int> = createObservable { emitter ->
             var i = 0
             while (i++ < 100) {
@@ -65,6 +150,32 @@ class FirstFragment : Fragment() {
                     Log.e("LOG_TAG", "error:$error")
                 })
         )
+    }
+
+    fun map() {
+        val observable: Observable<String> = createObservable { emitter ->
+            val currentDay = Calendar.getInstance().getDisplayName(
+                Calendar.DAY_OF_WEEK,
+                Calendar.LONG,
+                Locale.getDefault()
+            )
+            emitter.onNext("Today is $currentDay.")
+        }
+
+        val disposable = observable.map { currentDay ->
+            "$currentDay Time to go out"
+        }.schedule(
+            onNext = { item ->
+                binding.textviewFirst.text = item
+            },
+            onError = ::logError
+        )
+
+        linksToTask.add(disposable)
+    }
+
+    private fun logError(error: Throwable) {
+        Log.e("LOG_TAG", "error: $error")
     }
 
     fun start() {
--- app/src/main/java/rx_api/RxApi.kt
+++ app/src/main/java/rx_api/RxApi.kt
@@ -4,11 +4,44 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.BiFunction
 import io.reactivex.rxjava3.functions.Consumer
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun <T : Any> createObservable(source: ObservableOnSubscribe<T>): Observable<T> {
     return Observable.create(source)
+}
+
+fun <T : Any> createObservableFromIterable(data: Iterable<T>): Observable<T> {
+    return Observable.fromIterable(data)
+}
+
+fun <T : Any> createMergedObservables(
+    first: Observable<T>,
+    second: Observable<T>
+): Observable<T> {
+    return first.mergeWith(second)
+}
+
+fun <T : Any> Observable<T>.mergeTwoObservables(
+    observable: Observable<T>
+): Observable<T> {
+    return mergeWith(observable)
+}
+
+fun <T1: Any, T2: Any, R: Any> zipTwoObservables(
+    firstSource: Observable<T1>,
+    secondSource: Observable<T2>,
+    zipper: BiFunction<T1, T2, R>
+): Observable<R> {
+    return firstSource.zipWith(secondSource, zipper)
+}
+
+fun <T1: Any, T2: Any, R: Any> Observable<T1>.zipOnObservable(
+    source: Observable<T2>,
+    zipper: BiFunction<T1, T2, R>
+): Observable<R> {
+    return zipWith(source, zipper)
 }
 
 fun <T : Any> Observable<T>.schedule(
